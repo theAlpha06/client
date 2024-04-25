6c640c51e379b1406642bc914e6c3f7e974d25d2
Shubham <shubhamsinghmau2@gmail.com>
2024-04-25 20:51:38 +0530

    change cors

--- src/Services/apis.js
+++ src/Services/apis.js
@@ -1,22 +1,22 @@
 
 // AUTH ENDPOINTS
-// export const endpoints = {
-//   SIGNUP_API:   "https://server-qcrx.onrender.com/api/v2/auth/signup",
-//   LOGIN_API:   "https://server-qcrx.onrender.com/api/v2/auth/login",
-//   PROFILE_API: "https://server-qcrx.onrender.com/api/v2/userdetail/userDetail",
-//   SUBMISSION_API: "https://server-qcrx.onrender.com/api/v2/submission",
-//   UPDATE_API: "https://server-qcrx.onrender.com/api/v2/profile/updateProfile",
-//   LOGOUT_API: "https://server-qcrx.onrender.com/api/v2/auth/logout",
-// }
-
 export const endpoints = {
-  SIGNUP_API:   "http://localhost:3000/api/v2/auth/signup",
-  LOGIN_API:   "http://localhost:3000/api/v2/auth/login",
-  PROFILE_API: "http://localhost:3000/api/v2/userdetail/userDetail",
-  SUBMISSION_API: "http://localhost:3000/api/v2/submission",
-  UPDATE_API: "http://localhost:3000/api/v2/profile/updateProfile",
-  LOGOUT_API: "http://localhost:3000/api/v2/auth/logout",
+  SIGNUP_API:   "https://server-1-1dz0.onrender.com/api/v2/auth/signup",
+  LOGIN_API:   "https://server-1-1dz0.onrender.com/api/v2/auth/login",
+  PROFILE_API: "https://server-1-1dz0.onrender.com/api/v2/userdetail/userDetail",
+  SUBMISSION_API: "https://server-1-1dz0.onrender.com/api/v2/submission",
+  UPDATE_API: "https://server-1-1dz0.onrender.com/api/v2/profile/updateProfile",
+  LOGOUT_API: "https://server-1-1dz0.onrender.com/api/v2/auth/logout",
 }
+
+// export const endpoints = {
+//   SIGNUP_API:   "http://localhost:3000/api/v2/auth/signup",
+//   LOGIN_API:   "http://localhost:3000/api/v2/auth/login",
+//   PROFILE_API: "http://localhost:3000/api/v2/userdetail/userDetail",
+//   SUBMISSION_API: "http://localhost:3000/api/v2/submission",
+//   UPDATE_API: "http://localhost:3000/api/v2/profile/updateProfile",
+//   LOGOUT_API: "http://localhost:3000/api/v2/auth/logout",
+// }
 // ||||||| 870fc9b
 //   SIGNUP_API:   "https://server-qcrx.onrender.com/api/v2/auth/signup",
 //   LOGIN_API:   "https://server-qcrx.onrender.com/api/v2/auth/login",
